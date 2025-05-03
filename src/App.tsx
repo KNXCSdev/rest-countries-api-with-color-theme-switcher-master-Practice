9e7ba47b41bb79cde38c65d86948d571febd5464
KNXCSdev <michal.miksho@gmail.com>
2025-05-03 23:48:51 +0200

    react query to cache data

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppLayout from "./AppLayout";
 import Home from "./pages/Home";
 import { CountriesProvider } from "./context/CountriesContext";
 import Country from "./pages/Country";
 import DarkModeProvider from "./context/DarkModeContext";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <CountriesProvider>
-      <DarkModeProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<AppLayout />}>
-              <Route index element={<Navigate replace to="home" />} />
-              <Route path="home" element={<Home />} />{" "}
-              <Route path="home/:id" element={<Country />} />{" "}
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </DarkModeProvider>
-    </CountriesProvider>
+    <QueryClientProvider client={queryClient}>
+      <CountriesProvider>
+        <DarkModeProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<AppLayout />}>
+                <Route index element={<Navigate replace to="home" />} />
+                <Route path="home" element={<Home />} />
+                <Route path="home/:id" element={<Country />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </DarkModeProvider>
+      </CountriesProvider>
+    </QueryClientProvider>
   );
 }
 
